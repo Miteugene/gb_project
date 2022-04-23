e2385f48b1c5a4d7fb4340c9c1f6d81461c1557e
Evgenii Korotkov <miteugene@yandex.ru>
2022-04-23 22:36:28 +0100

    first routes

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello/{name}', function ( string $name ) {
+    return "Hello, {$name}!";
+})->where(['name' => '\w+']);
+
+Route::get('/info', function () {
+    return "Laravel project";
+});
+
+Route::get('/news/{news?}', function ( string $news = null ) {
+    if ( !$news )
+        return "Список всех новостей";
+
+    return "Конкретная новость: {$news}";
+})->where(['news' => '\w+']);
+
